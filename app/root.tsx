554a6e4885d38532ffb2eb9ab2b18abbe424e0e5
Nick Friedrich <nfriedrich1990@gmail.com>
2025-01-30 08:21:15 +0100

    .

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,7 +35,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div className="flex flex-col min-h-screen">
+          <div className="m-2">
+            <Header />
+          </div>
+          <div className="flex-grow p-4">{children}</div>
+          <Footer />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
